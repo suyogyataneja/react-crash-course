8a0becc3aa412d0c860f744ef33360f1b20c74d3
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-05-22 22:50:18 +1000

    Update the New Post component

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,29 +1,18 @@
 import classes from './NewPost.module.css'
 
-import { useState } from 'react';
 
-function NewPost(){
-
-    const [enteredBody, setEnteredBody] = useState('');
-    // let enteredBody ='';
-    function changeBodyHandler(event){
-
-        setEnteredBody(event.target.value);
-        // enteredBody = event.target.value;
-        // console.log(event.target.value);
-    }
-
+function NewPost(props){
     return(
 
         <form className={classes.form}>
             <p>
                 <label htmlFor="body"  >Text</label>
-                <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
+                <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
             </p>
-            <p>{enteredBody}</p>
+            {/* <p>{enteredBody}</p> */}
             <p>
                 <label htmlFor="name">Your Name </label>
-                <input type="text" id="name" required/>
+                <input type="text" id="name" required  onChange={props.onAuthorChange}/>
             </p>
 
 
