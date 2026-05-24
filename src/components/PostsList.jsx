52dcef3e71b72bcdeb99db0b1c5ee465a69b60f3
Suyogya Taneja <Suyogyataneja@gmail.com>
2026-05-24 23:08:38 +1000

    Introduce a reusable Modal with backdrop, a MainHeader with branding and "New  Post" button, and wire up state in PostsList to show/hide the new post form       inside the modal .

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,41 +1,23 @@
 import Post from './Post';
 import classes from './PostsList.module.css';
 import NewPost from './NewPost'
-import { useState } from 'react';
 import Modal from './Modal';
 
 function PostsList({isPosting, onStopPosting}){ 
     // const [enteredBody, setEnteredBody] = useState('');
    
-
-    const [enteredBody, setEnteredBody] = useState('');
-
-    const [enteredAuthor, setEnteredAuthor ] = useState('');
-
- 
-
-    function bodyChangeHandler(event){
-
-        setEnteredBody(event.target.value);
-    }
-
-    function AuthorChangeHandler(event){
-        setEnteredAuthor(event.target.value);
-
-    }
-
     return (
 
         <>
         {isPosting ?       <Modal onClose={onStopPosting}>
                 <NewPost
-                onBodyChange={bodyChangeHandler}  
-                onAuthorChange={AuthorChangeHandler} />
+                onCancel={onStopPosting}
+                />
                 </Modal>:false}
 
   
         <ul className={classes.posts}>
-            <Post author={enteredAuthor} body={enteredBody}/>
+            {/* <Post author={enteredAuthor} body={enteredBody}/> */}
             <Post author="Jane Smith" body="This is Jane's post."/>
             {/* <Post author="Bob Johnson" body="This is Bob's post."/>
             <Post author="Alice Williams" body="This is Alice's post."/> */}
